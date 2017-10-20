48665ea8a2c5a5fb5cf14bfd78286a1072f030d8
Carlos dos Santos <cds@cds-software.com.br>
2017-10-20 11:49:37 -0200

    MVCScaffold

---
+++ ExemploAula/ExemploAula/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ExemploAula.Models;
+
+namespace ExemploAula.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Mensagem = "Retornado pelo controler";
+            return View();
+        }
+
+        public IActionResult Lista()
+        {
+            var produtos = new List<Produto>();
+            produtos.Add(new Produto() { ID = 1, Nome = "Laranja", Preco = 2.5M });
+            produtos.Add(new Produto() { ID = 2, Nome = "Maça", Preco = 1.5M });
+            produtos.Add(new Produto() { ID = 3, Nome = "Banana", Preco = 5 });
+            produtos.Add(new Produto() { ID = 4, Nome = "Morango", Preco = 7 });
+            produtos.Add(new Produto() { ID = 5, Nome = "Jaboticaba", Preco = 4 });
+
+            return View(produtos);
+        }
+        
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ExemploAula/ExemploAula/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploAula.Models
+{
+    public class Produto
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
