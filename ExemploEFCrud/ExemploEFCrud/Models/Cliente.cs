0ff17cb1727c4725ce0a733169365732e711da7b
Carlos dos Santos <cds@cds-software.com.br>
2017-11-07 11:40:41 -0200

    exemplo crud ef

---
+++ ExemploEFCrud/ExemploEFCrud/Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ExemploEFCrud.Models
+{
+    [Table("CLIENTE")]
+    public class Cliente
+    {
+        [Key]
+        [Column("CLIENTEID")]
+        public int ClienteID { get; set; }
+        [Column("NOME")]
+        [Required(ErrorMessage ="Nao pode ser branco")]
+        //[MaxLength(50)]
+        public string Nome { get; set; }
+        [NotMapped]
+        public decimal Limite { get; set; }
+    }
+}
---
+++ ExemploEFCrud/ExemploEFCrud/Models/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ExemploEFCrud.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Cliente> Cliente { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("SYSTEM");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
